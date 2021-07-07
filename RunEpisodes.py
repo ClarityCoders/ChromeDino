27460e46af483bae9734c530a47fc8bc6ba61859
David Rodenkirchen <drodenkirchen@linetco.com>
2021-07-07 17:16:18 +0200

    improve if checks, add context manager for handling filestreams, remove prints

--- RunEpisodes.py
+++ RunEpisodes.py
@@ -4,12 +4,8 @@ import keyboard
 import random
 from utils.grabscreen import grab_screen
 
-# File for stats
-f = open("stats.csv", "w")
-
-
 time.sleep(5)
-keyboard.press('space')
+keyboard.press("space")
 keyboard.release("space")
 
 program_time = time.time()
@@ -26,12 +22,12 @@ for i in range(5000):
     start_speedup = random.randint(190, 210)
     count = 0
 
-    # Setting zero and white we will flip when game colors invert
-    zero = 0
+    # Initializing RGB values for colors black and white
+    black = 0
     white = 255
 
     # Starts game
-    keyboard.press('space')
+    keyboard.press("space")
     time.sleep(2)
     keyboard.release("space")
     episode_start = time.time()
@@ -40,30 +36,28 @@ for i in range(5000):
     # Bot rules you must update pixels!
     while True:
         last_time = time.time()
-        image = grab_screen(region=(85, 350, 715, 500))
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        image = cv2.cvtColor(grab_screen(region=(85, 350, 715, 500)), cv2.COLOR_BGR2GRAY)
         (thresh, image) = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY)
 
-        if image[144][376] == zero:
+        if image[144][376] == black:
             ahead += 4
-            holder = zero
-            zero = white
+            holder = black
+            black = white
             white = holder
 
         # Three cactus small
-        if image[92][ahead] == zero or image[92][ahead - 1] == zero or image[92][ahead - 2] == zero:
+        if sum([image[92][ahead - negative_ahead] for negative_ahead in range(3)]):
             keyboard.release("down")
             keyboard.press("space")
             time.sleep(0.01)
             keyboard.release("space")
 
         # DUCK
-        elif image[54][ahead] == zero:
-            #print("down")
+        elif image[54][ahead]:
             keyboard.press("down")
 
         # White background end test
-        if image[45][395] == white and image[35][370] == zero and image[35][420] == zero:
+        if image[45][395] and not image[35][370] and not image[35][420] == black:
             keyboard.release("space")
             keyboard.release("down")
             break
@@ -74,22 +68,22 @@ for i in range(5000):
             count = 0
             start = time.time()
             ahead += 1
-            #print(ahead)
 
     episode_score = int(time.time() - episode_start)
-    if len(episode_scores) > 0:
+    if len(episode_scores):
         if max(episode_scores) < episode_score:
             print(f"New Max Score: {episode_score}\nEpisode: {i}\nTime: {int(time.time() - program_time)}")
             print(f"Look Ahead {ahead}")
-    
-    f.write(f"{i},{episode_score},{start_ahead},{start_speedup}\n")
+
+    # Writing stats
+    with open("stats.csv", "w") as stats_file:
+        stats_file.write(f"{i},{episode_score},{start_ahead},{start_speedup}\n")
     print(f"{i},{episode_score},{start_ahead},{start_speedup}\n")
     episode_scores.append(episode_score)
-    keyboard.press('space')
+    keyboard.press("space")
     time.sleep(2)
     keyboard.release("space")
 
 print(f"Episodes: {len(episode_scores)}")
 print(f"Max: {max(episode_scores)}")
 print(f"Average: {sum(episode_scores) / len(episode_scores)}")
-f.close()
